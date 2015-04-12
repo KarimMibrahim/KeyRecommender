924c33c002a7d09416e9c77db5288e51c5316d6b
Ahmed Saad <ahmeds.elghareeb@gmail.com>
2015-04-12 12:07:45 +0200

    add files

--- App/search/views.py
+++ App/search/views.py
@@ -1,7 +1,44 @@
+#!/usr/bin/env python
+import os
+import re
+import urllib2
+import nltk.data
+import logging
+from bs4 import BeautifulSoup
+import numpy as np
+from gensim.models import Word2Vec
+from tempfile import TemporaryFile
+from sklearn.ensemble import RandomForestClassifier
+from KaggleWord2VecUtility import KaggleWord2VecUtility
+from sklearn.feature_extraction.text import CountVectorizer
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView
 from django.shortcuts import render
 from mails.models import ContactUs
+
+
+def makeFeatureVec(words, model, num_features):
+    featureVec = np.zeros((num_features,),dtype="float32")
+    nwords = 0.
+    index2word_set = set(model.index2word)
+    for word in words:
+        if word in index2word_set:
+            nwords = nwords + 1.
+            featureVec = np.add(featureVec,model[word])
+    featureVec = np.divide(featureVec,nwords)
+    return featureVec
+
+
+def getFeatureVectors(reviews, model, num_features):
+    counter = 0.
+    reviewFeatureVecs = np.zeros((len(reviews),num_features),dtype="float32")
+    for review in reviews:
+       if counter%1000. == 0.:
+           print "Review %d of %d" % (counter, len(reviews))
+       reviewFeatureVecs[counter] = makeFeatureVec(review, model, \
+           num_features)
+       counter = counter + 1.
+    return reviewFeatureVecs
 
 
 class HomePage(CreateView):
@@ -22,11 +59,11 @@ def dataset_list_view(request):
 
 def dataset_detail_view(request, dataset_id):
     # get the specific dataset by id
-    dataset = dataset_id  # get the dataset using its id
+    keywords = dataset_id  # get the dataset using its id
     return render(
         request,
         'search_results.html',
-        {"dataset": dataset})
+        {"keywords": keywords})
 
 # def search_view(request):
 #     if request.method == 'POST':
